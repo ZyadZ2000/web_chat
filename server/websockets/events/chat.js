ef81f940839156af8b436269c092ada950bb7000
Zyad Nezar <zyadenaba22@gmail.com>
2023-08-18 03:08:53 +0300

    added chat events

--- server/websockets/events/chat.js
+++ server/websockets/events/chat.js
@@ -1,6 +1,7 @@
 import * as chatHandlers from '../handlers/chat.js';
 import validate_fields from '../../utils/validation.js';
 import validationSchemas from '../../config/joi.js';
+
 export default function (socket) {
   socket.on('chat:sendMessage', async (data, cb) => {
     // In the case of a file it will be saved in the handler.
@@ -20,11 +21,60 @@ export default function (socket) {
     await chatHandlers.send_message(socket, data, cb);
   });
 
-  socket.on('chat:addAdmin');
-  socket.on('chat:removeAdmin');
-  socket.on('chat:removeMember');
-  socket.on('chat:changeName');
-  socket.on('chat:changePhoto');
-  socket.on('chat:leave');
-  socket.on('chat:delete');
+  socket.on('chat:addAdmin', handle_admin_add_or_remove(true));
+
+  socket.on('chat:removeAdmin', handle_admin_add_or_remove(false));
+
+  socket.on('chat:removeMember', async (data, cb) => {
+    const errors = validate_fields(['chatId', 'memberId'], data, {
+      chatId: validationSchemas.objectIdSchema,
+      memberId: validationSchemas.objectIdSchema,
+    });
+
+    if (Object.keys(errors).length === 0)
+      return cb({ success: false, error: errors });
+
+    await chatHandlers.remove_member(socket, data, cb);
+  });
+
+  socket.on('chat:changeName', handle_change_name_or_photo(true));
+  socket.on('chat:changePhoto', handle_change_name_or_photo(false));
+
+  socket.on('chat:leave', async (data, cb) => {
+    const errors = validate_fields(['chatId'], data, {
+      chatId: validationSchemas.objectIdSchema,
+    });
+
+    if (Object.keys(errors).length === 0)
+      return cb({ success: false, error: errors });
+
+    await chatHandlers.leave_chat(socket, data, cb);
+  });
+}
+
+function handle_admin_add_or_remove(isAdd) {
+  return async (data, cb) => {
+    const errors = validate_fields(['chatId', 'adminId'], data, {
+      chatId: validationSchemas.objectIdSchema,
+      adminId: validationSchemas.objectIdSchema,
+    });
+
+    if (Object.keys(errors).length === 0)
+      return cb({ success: false, error: errors });
+
+    await chatHandlers.add_or_remove_admin(socket, data, cb, isAdd);
+  };
+}
+
+function handle_change_name_or_photo(isName) {
+  return async (data, cb) => {
+    const errors = validate_fields(['chatId', 'chatName'], data, {
+      chatId: validationSchemas.objectIdSchema,
+      chatName: validationSchemas.nameSchema,
+    });
+
+    if (Object.keys(errors).length === 0)
+      return cb({ success: false, error: errors });
+    await chatHandlers.change_name_or_photo(socket, data, cb, isName);
+  };
 }
--- server/websockets/handlers/chat.js
+++ server/websockets/handlers/chat.js
@@ -1,5 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
-import { fileTypeFromFile } from 'file-type';
+import { fileTypeFromBuffer } from 'file-type';
+import mongoose from 'mongoose';
 
 // Node modules
 import fs from 'fs';
@@ -7,6 +8,7 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 
 import Chat from '../../models/chat.js';
+import User from '../../models/user.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -45,10 +47,17 @@ export async function send_message(socket, data, cb) {
         if (!file) throw new Error('No file provided');
 
         //check its type by calling get_valid_file_type();
-        const detectedFileType = get_valid_file_type(file);
+        const detectedFileType = await fileTypeFromBuffer(file);
 
+        if (
+          detectedFileType.ext !== 'png' &&
+          detectedFileType.ext !== 'jpg' &&
+          detectedFileType.ext !== 'mp4' &&
+          detectedFileType.ext !== 'mp3'
+        )
+          throw new Error('Invalid file type');
         //write it to disk, give the path of the file in the message.
-        const fileName = `${uuidv4()}.${detectedFileType}`;
+        const fileName = `${uuidv4()}.${detectedFileType.ext}`;
         const filePath = path.join(
           __dirname,
           '..',
@@ -103,6 +112,239 @@ export async function send_message(socket, data, cb) {
           },
         });
         break;
+    }
+  } catch (error) {
+    return cb({ success: false, error });
+  }
+}
+
+export async function add_or_remove_admin(socket, data, cb, isAdd) {
+  try {
+    const { chatId, adminId } = data;
+
+    const admin = await User.findById(adminId).select('_id');
+
+    if (!admin) throw new Error('User not found');
+
+    const chat = await Chat.aggregate([
+      {
+        $match: { _id: chatId },
+      },
+      {
+        $addFields: {
+          isCreator: {
+            $cond: {
+              if: { $eq: ['$creator', socket.user._id] },
+              then: true,
+              else: false,
+            },
+          },
+          isAdmin: {
+            $cond: {
+              if: {
+                $in: [admin._id, '$admins'],
+              },
+              then: true,
+              else: false,
+            },
+          },
+        },
+      },
+    ]);
+
+    if (!chat) throw new Error('Chat not found');
+    if (!chat.isCreator) throw new Error('Not authorized');
+    if (!chat.isAdmin && !isAdd) throw new Error('User is not an admin');
+    if (chat.isAdmin && isAdd) throw new Error('User is already an admin');
+
+    if (isAdd) {
+      chat.updateOne({ $push: { admins: admin._id } });
+    } else {
+      chat.updateOne({ $pull: { admins: admin._id } });
+    }
+
+    cb({ success: true });
+
+    return global.io
+      .to(chat._id)
+      .emit(isAdd ? 'chat:addAdmin' : 'chat:removeAdmin', {
+        adminId: admin._id,
+      });
+  } catch (error) {
+    return cb({ success: false, error });
+  }
+}
+
+export async function remove_member(socket, data, cb) {
+  const session = await mongoose.startSession();
+  session.abortTransaction();
+  try {
+    const { chatId, memberId } = data;
+
+    const member = await User.findById(memberId).select('_id chats');
+
+    if (!member) throw new Error('User not found');
+
+    const chat = await Chat.aggregate([
+      {
+        $match: { _id: chatId },
+      },
+      {
+        $addFields: {
+          isCreatorOrAdmin: {
+            $cond: {
+              if: {
+                $or: [
+                  { $eq: ['$creator', socket.user._id] },
+                  { $in: [socket.user._id, '$admins'] },
+                ],
+              },
+              then: true,
+              else: false,
+            },
+          },
+          isMember: {
+            $cond: {
+              if: { $in: [member._id, '$members'] },
+              then: true,
+              else: false,
+            },
+          },
+        },
+      },
+    ]);
+
+    if (!chat) throw new Error('Chat not found');
+    if (!chat.isCreatorOrAdmin) throw new Error('Not authorized');
+    if (!chat.isMember) throw new Error('User is not a member');
+
+    chat.updateOne({ $pull: { members: member._id } }).session(session);
+    member.updateOne({ $pull: { chats: chat._id } }).session(session);
+
+    await session.commitTransaction();
+    session.endSession();
+
+    cb({ success: true });
+
+    return global.io.to(chat._id).emit('chat:removeMember', {
+      memberId: member._id,
+    });
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    return cb({ success: false, error });
+  }
+}
+
+export async function leave_chat(socket, data, cb) {
+  const session = await mongoose.startSession();
+  session.abortTransaction();
+  try {
+    const { chatId } = data;
+    const chat = await Chat.aggregate([
+      {
+        $match: { _id: chatId },
+      },
+      {
+        $addFields: {
+          isMember: {
+            $cond: {
+              if: { $in: [socket.user._id, '$members'] },
+              then: true,
+              else: false,
+            },
+          },
+        },
+      },
+    ]);
+
+    if (!chat) throw new Error('Chat not found');
+    if (!chat.isMember) throw new Error('Not authorized');
+
+    chat.updateOne({ $pull: { members: socket.user._id } }).session(session);
+    socket.user.updateOne({ $pull: { chats: chat._id } }).session(session);
+
+    await session.commitTransaction();
+    session.endSession();
+
+    cb({ success: true });
+
+    return global.io.to(chat._id).emit('chat:leave', {
+      memberId: socket.user._id,
+    });
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    return cb({ success: false, error });
+  }
+}
+
+export async function change_name_or_photo(socket, data, cb, isName) {
+  try {
+    const { chatId, chatName, file } = data;
+    const chat = await Chat.aggregate([
+      {
+        $match: { _id: chatId },
+      },
+      {
+        $addFields: {
+          isCreatorOrAdmin: {
+            $cond: {
+              if: {
+                $or: [
+                  { $eq: ['$creator', socket.user._id] },
+                  { $in: [socket.user._id, '$admins'] },
+                ],
+              },
+              then: true,
+              else: false,
+            },
+          },
+        },
+      },
+    ]);
+
+    if (!chat) throw new Error('Chat not found');
+    if (!chat.isCreatorOrAdmin) throw new Error('Not authorized');
+
+    if (isName) {
+      await chat.updateOne({ $set: { name: chatName } });
+      cb({ success: true });
+      return global.io.to(chat._id).emit('chat:changeName', { name: chatName });
+    } else {
+      if (!file) throw new Error('No file provided');
+      const fileType = await fileTypeFromBuffer(file);
+      if (fileType.ext !== 'png' && fileType.ext !== 'jpg')
+        throw new Error('Invalid file type');
+
+      const fileName = `${uuidv4()}.${fileType.ext}`;
+      const filePath = path.join(
+        __dirname,
+        '..',
+        '..',
+        'public',
+        'assets',
+        fileName
+      );
+
+      const writeStream = fs.createWriteStream(filePath);
+
+      writeStream.on('finish', async () => {
+        await chat.updateOne({
+          $set: {
+            photo: fileName,
+          },
+        });
+
+        cb({ success: true });
+
+        return global.io.to(chat._id).emit('chat:changePhoto', {
+          photo: fileName,
+        });
+      });
+
+      writeStream.write(data.buffer);
+      writeStream.end();
     }
   } catch (error) {
     return cb({ success: false, error });
